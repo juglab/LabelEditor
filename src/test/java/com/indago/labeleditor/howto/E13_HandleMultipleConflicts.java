3a8093911f65f793571593ce4d28885ed6abef92
frauzufall <mail@frauzufall.de>
2019-11-07 23:49:08 +0100

    Example for multiple conflict sets

---
+++ src/test/java/com/indago/labeleditor/howto/E13_HandleMultipleConflicts.java
@@ -0,0 +1,97 @@
+package com.indago.labeleditor.howto;
+
+
+import com.indago.labeleditor.LabelEditorBdvPanel;
+import com.indago.labeleditor.LabelEditorPanel;
+import com.indago.labeleditor.model.DefaultLabelEditorModel;
+import com.indago.labeleditor.model.LabelEditorModel;
+import com.indago.labeleditor.model.LabelEditorTag;
+import net.imglib2.RandomAccess;
+import net.imglib2.algorithm.region.hypersphere.HyperSphere;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.IntArray;
+import net.imglib2.roi.labeling.ImgLabeling;
+import net.imglib2.roi.labeling.LabelingType;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.Views;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * How to open an {@link ImgLabeling} in a {@link LabelEditorBdvPanel}.
+ */
+public class E13_HandleMultipleConflicts {
+
+	static JFrame frame = new JFrame("Label editor");
+	static LabelEditorPanel<String> panel;
+
+	@Test
+	public void run() {
+
+		String LABEL1 = "label1";
+		String LABEL2 = "label2";
+		String LABEL3 = "label3";
+		String LABEL4 = "label4";
+		String LABEL5 = "label5";
+		String LABEL6 = "label6";
+		String LABEL7 = "label7";
+		String LABEL8 = "label8";
+
+		String TAG1 = "tag1";
+
+		ArrayImg<IntType, IntArray> backing = ArrayImgs.ints( 500, 500 );
+		ImgLabeling< String, IntType > labels = new ImgLabeling<>( backing );
+
+		int radius = 50;
+		drawSphere(labels, new long[]{170, 170}, radius, LABEL1);
+		drawSphere(labels, new long[]{170, 200}, radius, LABEL2);
+		drawSphere(labels, new long[]{200, 200}, radius, LABEL3);
+		drawSphere(labels, new long[]{200, 170}, radius, LABEL4);
+
+		drawSphere(labels, new long[]{270, 370}, radius, LABEL5);
+		drawSphere(labels, new long[]{270, 400}, radius, LABEL6);
+		drawSphere(labels, new long[]{300, 400}, radius, LABEL7);
+		drawSphere(labels, new long[]{300, 370}, radius, LABEL8);
+
+		LabelEditorModel<String> model = new DefaultLabelEditorModel<>(labels);
+
+		model.tagging().addTag(LabelEditorTag.SELECTED, LABEL2);
+		model.tagging().addTag(LabelEditorTag.SELECTED, LABEL7);
+
+		panel = new LabelEditorBdvPanel<>();
+		panel.init(model);
+		panel.rendering().setTagColor(LabelEditorTag.SELECTED, ARGBType.rgba(255,0,0,255));
+		panel.rendering().setTagColor(TAG1, ARGBType.rgba(255,255,255,100));
+		panel.updateLabelRendering();
+		frame.setContentPane(panel.get());
+		frame.setMinimumSize(new Dimension(500,500));
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private void drawSphere(ImgLabeling<String, IntType> img, long[] position, int radius, String label) {
+		RandomAccess<LabelingType<String>> ra = img.randomAccess();
+		ra.setPosition(position);
+		HyperSphere<LabelingType<String>> hyperSphere = new HyperSphere<>(img, ra, radius);
+		for (LabelingType<String> value : hyperSphere)
+			value.add(label);
+	}
+
+	@AfterClass
+	public static void dispose() {
+		frame.dispose();
+		panel.dispose();
+	}
+
+	public static void main(String... args) {
+		new E13_HandleMultipleConflicts().run();
+	}
+
+}
