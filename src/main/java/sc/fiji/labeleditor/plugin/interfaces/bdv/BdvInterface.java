d9fa9347e13dad0e23596cf64dce8eb88d242397
frauzufall <mail@frauzufall.de>
2019-12-13 16:26:05 +0100

    BdvInterface: use BdvHandle instead of BdvHandlePanel (fixes #16)

--- src/main/java/sc/fiji/labeleditor/plugin/interfaces/bdv/BdvInterface.java
+++ src/main/java/sc/fiji/labeleditor/plugin/interfaces/bdv/BdvInterface.java
@@ -1,8 +1,19 @@
 package sc.fiji.labeleditor.plugin.interfaces.bdv;
 
+import bdv.util.BdvHandle;
 import bdv.util.BdvHandlePanel;
 import bdv.util.BdvSource;
 import bdv.viewer.ViewerPanel;
+import net.imglib2.Localizable;
+import net.imglib2.Point;
+import net.imglib2.RandomAccess;
+import net.imglib2.RealPoint;
+import net.imglib2.roi.labeling.LabelingType;
+import net.imglib2.util.Intervals;
+import org.scijava.Context;
+import org.scijava.plugin.Parameter;
+import org.scijava.ui.behaviour.io.InputTriggerConfig;
+import org.scijava.ui.behaviour.util.Behaviours;
 import sc.fiji.labeleditor.core.controller.DefaultLabelEditorController;
 import sc.fiji.labeleditor.core.controller.LabelEditorBehaviours;
 import sc.fiji.labeleditor.core.controller.LabelEditorController;
@@ -15,16 +26,6 @@ import sc.fiji.labeleditor.plugin.behaviours.FocusBehaviours;
 import sc.fiji.labeleditor.plugin.behaviours.PopupBehaviours;
 import sc.fiji.labeleditor.plugin.behaviours.modification.LabelingModificationBehaviours;
 import sc.fiji.labeleditor.plugin.behaviours.select.SelectionBehaviours;
-import net.imglib2.Localizable;
-import net.imglib2.Point;
-import net.imglib2.RandomAccess;
-import net.imglib2.RealPoint;
-import net.imglib2.roi.labeling.LabelingType;
-import net.imglib2.util.Intervals;
-import org.scijava.Context;
-import org.scijava.plugin.Parameter;
-import org.scijava.ui.behaviour.io.InputTriggerConfig;
-import org.scijava.ui.behaviour.util.Behaviours;
 
 import javax.swing.*;
 import java.awt.*;
@@ -36,24 +37,24 @@ public class BdvInterface<L> implements LabelEditorInterface<L> {
 	@Parameter
 	Context context;
 
-	private final BdvHandlePanel panel;
+	private final BdvHandle bdvHandle;
 	private final List<BdvSource> sources;
 	private final Behaviours behaviours;
 	private final LabelEditorView<L> view;
 	private boolean mode3D;
 	private LabelingType<L> labelsAtCursor;
 
-	public BdvInterface(BdvHandlePanel panel, List<BdvSource> bdvSources, LabelEditorView view) {
-		this.panel = panel;
+	public BdvInterface(BdvHandle bdvHandle, List<BdvSource> bdvSources, LabelEditorView view) {
+		this.bdvHandle = bdvHandle;
 		this.sources = bdvSources;
 		this.view = view;
 		this.behaviours = new Behaviours(new InputTriggerConfig(), "labeleditor");
-		behaviours.install(panel.getTriggerbindings(), "labeleditor");
+		behaviours.install(this.bdvHandle.getTriggerbindings(), "labeleditor");
 	}
 
-	public static <L> LabelEditorController control(LabelEditorModel<L> model, LabelEditorView<L> view, BdvHandlePanel panel) {
+	public static <L> LabelEditorController control(LabelEditorModel<L> model, LabelEditorView<L> view, BdvHandle bdvHandle) {
 		LabelEditorController<L> controller = new DefaultLabelEditorController<>();
-		controller.init(model, view, new BdvInterface<>(panel, null, view));
+		controller.init(model, view, new BdvInterface<>(bdvHandle, null, view));
 		controller.addDefaultBehaviours();
 		controller.interfaceInstance().set3DViewMode(false);
 		return controller;
@@ -74,10 +75,10 @@ public class BdvInterface<L> implements LabelEditorInterface<L> {
 				return labelsAtCursor;
 			}
 			this.labelsAtCursor = ra.get();
-			panel.getViewerPanel().getDisplay().setToolTipText(view.getToolTip(labelsAtCursor));
+			bdvHandle.getViewerPanel().getDisplay().setToolTipText(view.getToolTip(labelsAtCursor));
 			return labelsAtCursor;
 		}
-		panel.getViewerPanel().getDisplay().setToolTipText(null);
+		bdvHandle.getViewerPanel().getDisplay().setToolTipText(null);
 		return null;
 	}
 
@@ -89,10 +90,10 @@ public class BdvInterface<L> implements LabelEditorInterface<L> {
 	private Localizable getDataPositionAtMouse() {
 		//FIXME currently only works for 2D, 3D and 4D
 		RealPoint mousePointer = new RealPoint(3);
-		panel.getViewerPanel().getGlobalMouseCoordinates( mousePointer );
+		bdvHandle.getViewerPanel().getGlobalMouseCoordinates( mousePointer );
 		final int x = ( int ) mousePointer.getFloatPosition( 0 );
 		final int y = ( int ) mousePointer.getFloatPosition( 1 );
-		int time = panel.getViewerPanel().getState().getCurrentTimepoint();
+		int time = bdvHandle.getViewerPanel().getState().getCurrentTimepoint();
 		if(mode3D) {
 			final int z = ( int ) mousePointer.getFloatPosition( 2 );
 			return new Point(x, y, z, time);
@@ -102,7 +103,7 @@ public class BdvInterface<L> implements LabelEditorInterface<L> {
 
 	@Override
 	public void installBehaviours(LabelEditorModel<L> model, LabelEditorController<L> controller, LabelEditorView<L> view) {
-		install(model, controller, new SelectionBehaviours<>());
+		install(model, controller, new SelectionBehaviours());
 		install(model, controller, new FocusBehaviours<>());
 		install(model, controller, new LabelingModificationBehaviours());
 		install(model, controller, new PopupBehaviours());
@@ -111,12 +112,12 @@ public class BdvInterface<L> implements LabelEditorInterface<L> {
 	private void install(LabelEditorModel<L> model, LabelEditorController<L> controller, LabelEditorBehaviours behavioursAdded) {
 		if(context != null) context.inject(behavioursAdded);
 		behavioursAdded.init(model, controller, view);
-		behavioursAdded.install(behaviours, panel.getViewerPanel().getDisplay());
+		behavioursAdded.install(behaviours, bdvHandle.getViewerPanel().getDisplay());
 	}
 
 	@Override
 	public void onViewChange(ViewChangedEvent viewChangedEvent) {
-		panel.getViewerPanel().requestRepaint();
+		bdvHandle.getViewerPanel().requestRepaint();
 	}
 
 	@Override
@@ -126,14 +127,14 @@ public class BdvInterface<L> implements LabelEditorInterface<L> {
 
 	@Override
 	public ViewerPanel getComponent() {
-		return panel.getViewerPanel();
+		return bdvHandle.getViewerPanel();
 	}
 
 	@Override
 	public void onTagChange(List<TagChangedEvent> tagChangedEvents) {
 		if(labelsAtCursor == null) return;
-		panel.getViewerPanel().getDisplay().setToolTipText(view.getToolTip(labelsAtCursor));
-		showToolTip(panel.getViewerPanel().getDisplay());
+		bdvHandle.getViewerPanel().getDisplay().setToolTipText(view.getToolTip(labelsAtCursor));
+		showToolTip(bdvHandle.getViewerPanel().getDisplay());
 	}
 
 	public static void showToolTip(JComponent component) {
