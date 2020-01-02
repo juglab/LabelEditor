76e04a246a04bebd36b7603882128f773b0e794a
frauzufall <mail@frauzufall.de>
2020-01-02 13:24:20 +0100

    LabelEditorValueColor: Fix color for values below min val and above max val
    * if a value is below min, it will get the color of min
    * if a value is above max, it will get the color of max

---
+++ src/test/java/sc/fiji/labeleditor/core/model/colors/ValueColorTest.java
@@ -0,0 +1,25 @@
+package sc.fiji.labeleditor.core.model.colors;
+
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.integer.IntType;
+import org.junit.Test;
+import sc.fiji.labeleditor.core.view.LabelEditorTargetComponent;
+
+import static org.junit.Assert.assertEquals;
+
+public class ValueColorTest {
+
+	@Test
+	public void testValueTag() {
+		LabelEditorColorset colorset = new LabelEditorColorset(null);
+		LabelEditorValueColor<IntType> color = new LabelEditorValueColor<>(colorset, new IntType(0), new IntType(100));
+		colorset.put(LabelEditorTargetComponent.FACE, color);
+		color.setMinColor(0,0,255,255);
+		color.setMaxColor(255,0,0,255);
+
+		assertEquals(ARGBType.rgba(0, 0, 255, 255), color.getColor(new IntType(0)));
+		assertEquals(ARGBType.rgba(255/2, 0, 255/2, 255), color.getColor(new IntType(50)));
+		assertEquals(ARGBType.rgba(255, 0, 0, 255), color.getColor(new IntType(100)));
+		assertEquals(ARGBType.rgba(255, 0, 0, 255), color.getColor(new IntType(150)));
+	}
+}
