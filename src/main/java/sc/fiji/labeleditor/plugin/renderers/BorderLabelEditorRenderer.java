7715c3e68a747b577f5ecd16a467c72781b232bb
frauzufall <mail@frauzufall.de>
2020-05-11 18:05:00 +0200

    LabelEditorRenderer: Add generic types

--- src/main/java/sc/fiji/labeleditor/plugin/renderers/BorderLabelEditorRenderer.java
+++ src/main/java/sc/fiji/labeleditor/plugin/renderers/BorderLabelEditorRenderer.java
@@ -1,8 +1,5 @@
 package sc.fiji.labeleditor.plugin.renderers;
 
-import sc.fiji.labeleditor.core.model.LabelEditorModel;
-import sc.fiji.labeleditor.core.view.LabelEditorRenderer;
-import sc.fiji.labeleditor.core.view.LabelEditorTargetComponent;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.converter.Converter;
 import net.imglib2.converter.Converters;
@@ -10,16 +7,19 @@ import net.imglib2.roi.boundary.IntTypeBoundary;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.integer.IntType;
 import org.scijava.plugin.Plugin;
+import sc.fiji.labeleditor.core.model.LabelEditorModel;
+import sc.fiji.labeleditor.core.view.LabelEditorRenderer;
+import sc.fiji.labeleditor.core.view.LabelEditorTargetComponent;
 
 @Plugin(type = LabelEditorRenderer.class, name = "borders", priority = 2)
 public class BorderLabelEditorRenderer<L> extends DefaultLabelEditorRenderer<L> {
 
-	private RandomAccessibleInterval output;
+	private RandomAccessibleInterval<IntType> output;
 
 	@Override
 	public void init(LabelEditorModel<L> model) {
 		super.init(model);
-		this.output = new IntTypeBoundary(model.labeling().getIndexImg(), -1);
+		this.output = new IntTypeBoundary<>(model.labeling().getIndexImg(), -1);
 	}
 
 	@Override
