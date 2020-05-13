e0d324b283b2794fba8baa40d7be96e72bff9344
frauzufall <mail@frauzufall.de>
2020-05-13 09:27:40 +0200

    fix LabelEditorTable

--- src/main/java/sc/fiji/labeleditor/plugin/table/LabelEditorTable.java
+++ src/main/java/sc/fiji/labeleditor/plugin/table/LabelEditorTable.java
@@ -3,8 +3,11 @@ package sc.fiji.labeleditor.plugin.table;
 import org.scijava.table.interactive.DefaultInteractiveTable;
 import org.scijava.table.interactive.SelectionModelAdapter;
 import sc.fiji.labeleditor.core.controller.InteractiveLabeling;
+import sc.fiji.labeleditor.core.model.tagging.LabelEditorTag;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class LabelEditorTable<L> extends DefaultInteractiveTable<Integer> {
@@ -14,18 +17,22 @@ public class LabelEditorTable<L> extends DefaultInteractiveTable<Integer> {
 		Map<Integer, L> mappingLeft = new HashMap<>();
 		Map<L, Integer> mappingRight = new HashMap<>();
 		int i = 0;
+		List<LabelEditorTag> labelEditorTags = Arrays.asList(LabelEditorTag.values());
 		for (L label : labeling.model().labeling().getMapping().getLabels()) {
-			appendRow();
+			appendRow(label.toString());
 			for (Object tag : labeling.model().tagging().getTags(label)) {
-				if(getColumnIndex(tag.toString()) < 0) {
+				if(labelEditorTags.contains(tag)) continue;
+				int col = getColumnIndex(tag.toString());
+				if(col < 0) {
 					appendColumn(tag.toString());
+					col = getColumnCount()-1;
 				}
 				Object value = labeling.model().tagging().getValue(tag, label);
-				if(value != null) set(tag.toString(), i, value.toString());
+				if(value != null) set(col, i, value.toString());
 			}
-			i++;
 			mappingLeft.put(i, label);
 			mappingRight.put(label, i);
+			i++;
 		}
 
 		setSelectionModel(new SelectionModelAdapter<>(labeling.getSelectionModel(), mappingRight, mappingLeft));
