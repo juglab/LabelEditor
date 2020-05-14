b45bbad65130b5120fd6e3fc908d04da80c4bc2f
frauzufall <mail@frauzufall.de>
2020-05-14 18:40:52 +0200

    Delete labels: speed up

--- src/main/java/sc/fiji/labeleditor/plugin/behaviours/modification/DeleteLabels.java
+++ src/main/java/sc/fiji/labeleditor/plugin/behaviours/modification/DeleteLabels.java
@@ -3,6 +3,9 @@ package sc.fiji.labeleditor.plugin.behaviours.modification;
 import net.imglib2.Cursor;
 import net.imglib2.IterableInterval;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.loops.LoopBuilder;
+import net.imglib2.roi.Regions;
+import net.imglib2.roi.labeling.LabelRegions;
 import net.imglib2.roi.labeling.LabelingType;
 import net.imglib2.view.Views;
 import org.scijava.ui.behaviour.Behaviour;
@@ -26,18 +29,16 @@ public class DeleteLabels<L> implements Behaviour {
 	}
 
 	private static <L> void delete(Set<L> labels, RandomAccessibleInterval<LabelingType<L>> labeling) {
-		Cursor<LabelingType<L>> cursor = Views.iterable(labeling).cursor();
-		while (cursor.hasNext()) {
-			LabelingType<L> val = cursor.next();
-			val.removeAll(labels);
+		LabelRegions<L> regions = new LabelRegions<>(labeling);
+		for (L label : labels) {
+			IterableInterval<LabelingType<L>> sample = Regions.sample(regions.getLabelRegion(label), labeling);
+			sample.forEach(pixel -> pixel.remove(label));
 		}
 	}
 
-	static <L> void delete(L label, IterableInterval<LabelingType<L>> labeling) {
-		Cursor<LabelingType<L>> cursor = labeling.cursor();
-		while (cursor.hasNext()) {
-			LabelingType<L> val = cursor.next();
-			val.remove(label);
-		}
+	static <L> void delete(L label, RandomAccessibleInterval<LabelingType<L>> labeling) {
+		LabelRegions<L> regions = new LabelRegions<>(labeling);
+		IterableInterval<LabelingType<L>> sample = Regions.sample(regions.getLabelRegion(label), labeling);
+		sample.forEach(pixel -> pixel.remove(label));
 	}
 }
