bd173016f6ef7733af7dc3757b583e247abe09b0
frauzufall <mail@frauzufall.de>
2020-05-14 18:40:35 +0200

    LabelEditorTable: Add notes, listen to tag changes

--- src/main/java/sc/fiji/labeleditor/plugin/table/LabelEditorTable.java
+++ src/main/java/sc/fiji/labeleditor/plugin/table/LabelEditorTable.java
@@ -4,16 +4,28 @@ import org.scijava.table.interactive.DefaultInteractiveTable;
 import org.scijava.table.interactive.SelectionModelAdapter;
 import sc.fiji.labeleditor.core.controller.InteractiveLabeling;
 import sc.fiji.labeleditor.core.model.tagging.LabelEditorTag;
+import sc.fiji.labeleditor.core.model.tagging.TagChangeListener;
+import sc.fiji.labeleditor.core.model.tagging.TagChangedEvent;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class LabelEditorTable<L> extends DefaultInteractiveTable<Integer> {
+//TODO since the DefaultLabelEditorTagging is already a table, it might be better to display it directly
+public class LabelEditorTable<L> extends DefaultInteractiveTable<Integer> implements TagChangeListener {
+
+	private final InteractiveLabeling<L> labeling;
 
 	public LabelEditorTable(InteractiveLabeling<L> labeling) {
 
+		this.labeling = labeling;
+
+		populateTable();
+		labeling.model().tagging().listeners().add(this);
+	}
+
+	private void populateTable() {
 		Map<Integer, L> mappingLeft = new HashMap<>();
 		Map<L, Integer> mappingRight = new HashMap<>();
 		int i = 0;
@@ -34,7 +46,14 @@ public class LabelEditorTable<L> extends DefaultInteractiveTable<Integer> {
 			mappingRight.put(label, i);
 			i++;
 		}
-
 		setSelectionModel(new SelectionModelAdapter<>(labeling.getSelectionModel(), mappingRight, mappingLeft));
+	}
+
+	@Override
+	public void tagChanged(List<TagChangedEvent> e) {
+		e.forEach(event -> {
+			if(event.model != labeling.model()) return;
+			//TODO update table entry
+		});
 	}
 }
