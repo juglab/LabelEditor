a437defbb84c41be2992df422528fdbb2f199506
Deborah Schmidt <mail@frauzufall.de>
2022-12-22 00:22:32 +0100

    Adjust default colors

--- src/main/java/sc/fiji/labeleditor/core/model/DefaultColors.java
+++ src/main/java/sc/fiji/labeleditor/core/model/DefaultColors.java
@@ -33,7 +33,7 @@ import net.imglib2.type.numeric.ARGBType;
 public class DefaultColors {
 	public static int selectedFace() { return ARGBType.rgba(255,255,255,133); }
 	public static int selectedBorder() { return ARGBType.rgba(255,255,255,200); }
-	public static int defaultFace() { return ARGBType.rgba(0, 255, 99, 133); }
+	public static int defaultFace() { return ARGBType.rgba(0, 255, 99, 33); }
 	public static int defaultBorder() { return ARGBType.rgba(0, 255, 99, 250); }
 	public static int focusBorder() { return ARGBType.rgba(200,200,200,200); }
 	public static int focusFace() { return ARGBType.rgba(255,255,255,77); }
